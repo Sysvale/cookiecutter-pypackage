2895f5ad6d8cfb2b29de39dddff64dfed76c047c
elaynelemos <elayne.l.lemos@gmail.com>
2020-06-03 13:28:09 -0300

    Make adjusts to code style

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -1,8 +1,8 @@
 import traceback
 from {{cookiecutter.project_slug}}.{{cookiecutter.project_slug}} import main
 
-if __name__ == '__main__':
-  try:
-    main()
-  except Exception as e:
-    print(traceback.format_exc())
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        print(traceback.format_exc())
